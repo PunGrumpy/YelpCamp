d3c9df28647126542ba1bb3bb8db831e678af4e8
PunGrumpy <noppakorn.pun@gmail.com>
2022-09-14 11:24:28 +0700

    modified:   controllers/campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -53,7 +53,6 @@ module.exports.renderEditForm = async (req, res) => {
 
 module.exports.updateCampground = async (req, res) => {
     const { id } = req.params;
-    console.log(req.body);
     const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
